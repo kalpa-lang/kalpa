b13e62e5438afa866ccae388bf56f231c2d111e0
Roman Zhikharevich <rzhikharevich@gmail.com>
2020-01-18 20:55:12 +0300

    Add file I/O to main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,76 @@
-#include <fmt/format.h>
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <optional>
+#include <vector>
+
+#include "print.h"
 
 
-int main() {
-    fmt::print(FMT_STRING("Hello, {}!"), "World");
+namespace klp {
+
+
+void print_file_error(const char* path) {
+    eprint("Error: {}: {}\n", path, std::strerror(errno));
+}
+
+
+std::optional<std::vector<char>> read_file(const char* path) {
+    std::optional<std::vector<char>> ret;
+
+    const auto file = std::fopen(path, "r");
+    if (!file) {
+        print_file_error(path);
+        return ret;
+    }
+
+    if (std::fseek(file, 0, SEEK_END) < 0) {
+        print_file_error(path);
+        return ret;
+    }
+
+    const auto file_size = std::ftell(file);
+    if (file_size < 0) {
+        print_file_error(path);
+        return ret;
+    }
+
+    if (std::fseek(file, 0, SEEK_SET) < 0) {
+        print_file_error(path);
+        return ret;
+    }
+
+    std::vector<char> file_bytes(file_size);
+    if (std::fread(file_bytes.data(), file_size, 1, file) != 1) {
+        print_file_error(path);
+        return ret;
+    }
+
+    ret = std::move(file_bytes);
+    return ret;
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        eputs("Usage: kalpa <path/to/source.kl>");
+        return 1;
+    }
+
+    const auto source = read_file(argv[1]);
+    if (!source) {
+        return 1;
+    }
+
+    print("source size: {}\n", source->size());
+
+    return 0;
+}
+
+
+}
+
+
+int main(int argc, char* argv[]) {
+    return klp::main(argc, argv);
 }
---
+++ src/print.h
@@ -0,0 +1,33 @@
+#ifndef KALPA_PRINT_H
+#define KALPA_PRINT_H
+
+
+#include <string_view>
+
+#include <fmt/format.h>
+
+
+namespace klp {
+
+
+using std::puts;
+
+using fmt::format;
+using fmt::print;
+
+
+static void eputs(const char* msg) {
+    fmt::print(stderr, "{}\n", msg);
+}
+
+
+template <typename... Args>
+void eprint(std::string_view format, const Args&... args) {
+    fmt::print(stderr, format, args...);
+}
+
+
+}
+
+
+#endif

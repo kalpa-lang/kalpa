39ce3e65aa1f919152f03d25ad23ff0016ac36b2
root <abarankab@gmail.com>
2020-01-19 15:39:34 +0300

    main.cc now tests the tokenizer

--- src/main.cc
+++ src/main.cc
@@ -57,6 +57,60 @@ std::optional<std::vector<char>> read_file(const char* path) {
     return ret;
 }
 
+void print_token(Token& token) {
+    switch (token.type) {
+        case Token::Type::Identifier            : eputs("Identifier"); break;
+        case Token::Type::Indent                : eputs("Indent"); break;
+        case Token::Type::Dedent                : eputs("Dedent"); break;
+        case Token::Type::LeftParen             : eputs("LeftParen"); break;
+        case Token::Type::RightParen            : eputs("RightParen"); break;
+        case Token::Type::Colon         : eputs("Colon"); break;
+        case Token::Type::Comma         : eputs("Comma"); break;
+        case Token::Type::Dot           : eputs("Dot"); break;
+        case Token::Type::Def           : eputs("Def"); break;
+        case Token::Type::Class         : eputs("Class"); break;
+        case Token::Type::Let           : eputs("Let"); break;
+        case Token::Type::For           : eputs("For"); break;
+        case Token::Type::While         : eputs("While"); break;
+        case Token::Type::If            : eputs("If"); break;
+        case Token::Type::Else          : eputs("Else"); break;
+        case Token::Type::Elif          : eputs("Elif"); break;
+        case Token::Type::In            : eputs("In"); break;
+        case Token::Type::Not           : eputs("Not"); break;
+        case Token::Type::Or            : eputs("Or"); break;
+        case Token::Type::And           : eputs("And"); break;
+        case Token::Type::Return                : eputs("Return"); break;
+        case Token::Type::Int           : eputs("Int"); break;
+        case Token::Type::Float         : eputs("Float"); break;
+        case Token::Type::String                : eputs("String"); break;
+        case Token::Type::Assign                : eputs("Assign"); break;
+        case Token::Type::Equal         : eputs("Equal"); break;
+        case Token::Type::NotEqual              : eputs("NotEqual"); break;
+        case Token::Type::Less          : eputs("Less"); break;
+        case Token::Type::LessEq                : eputs("LessEq"); break;
+        case Token::Type::Greater               : eputs("Greater"); break;
+        case Token::Type::GreaterEq             : eputs("GreaterEq"); break;
+        case Token::Type::Add           : eputs("Add"); break;
+        case Token::Type::Sub           : eputs("Sub"); break;
+        case Token::Type::Mul           : eputs("Mul"); break;
+        case Token::Type::Pow           : eputs("Pow"); break;
+        case Token::Type::Div           : eputs("Div"); break;
+        case Token::Type::IntDiv                : eputs("IntDiv"); break;
+        case Token::Type::AddEq         : eputs("AddEq"); break;
+        case Token::Type::SubEq         : eputs("SubEq"); break;
+        case Token::Type::MulEq         : eputs("MulEq"); break;
+        case Token::Type::PowEq         : eputs("PowEq"); break;
+        case Token::Type::DivEq         : eputs("DivEq"); break;
+        case Token::Type::IntDivEq              : eputs("IntDivEq"); break;
+        case Token::Type::Xor           : eputs("Xor"); break;
+        case Token::Type::XorEq         : eputs("XorEq"); break;
+        case Token::Type::LeftBracket           : eputs("LeftBracket"); break;
+        case Token::Type::RightBracket          : eputs("RightBracket"); break;
+        case Token::Type::LeftBrace             : eputs("LeftBrace"); break;
+        case Token::Type::RightBrace            : eputs("RightBrace"); break;
+        case Token::Type::Eof           : eputs("Eof"); break;
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -75,13 +129,20 @@ int main(int argc, char* argv[]) {
 
     while (true) {
         auto token = tokenizer.next();
+        print_token(token);
         if (token.type == Token::Type::Eof) {
             break;
         }
 
+        if (token.type == Token::Type::Identifier ||
+            token.type == Token::Type::String ||
+            token.type == Token::Type::Int ||
+            token.type == Token::Type::Float)
         std::visit([] (auto& x) {
             print("{}\n", x);
         }, token.value);
+
+        eputs("---");
     }
 
     return 0;

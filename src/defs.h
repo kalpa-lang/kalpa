03d9025e88c3cf036a7959dfaef048ceb9901114
Roman Zhikharevich <rzhikharevich@gmail.com>
2020-02-02 21:06:59 +0300

    Improve Result<V, E>

--- src/defs.h
+++ src/defs.h
@@ -11,11 +11,19 @@
 namespace klp {
 
 
+#define KALPA_IDENTITY(x) (x)
+
+
+#define KALPA_DO_STRINGIFY(s) #s
+#define KALPA_STRINGIFY(s) KALPA_DO_STRINGIFY(s)
+
+
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
 using usize = std::size_t;
+
 
 using i8 = std::int8_t;
 using i16 = std::int16_t;
@@ -26,6 +34,7 @@ using isize = std::intptr_t;
 
 void verify(bool cond, const char* msg = nullptr);
 
+
 template <typename T, typename U>
 void verify_eq(const T& x, const U& y) {
     if (x != y) {
@@ -34,14 +43,23 @@ void verify_eq(const T& x, const U& y) {
     }
 }
 
+
+#define KALPA_VERIFY(cond) verify(cond, KALPA_STRINGIFY(cond))
+
+
 [[noreturn]]
 void todo(const char* msg = nullptr);
 
 
-#define KALPA_DO_STRINGIFY(s) #s
-#define KALPA_STRINGIFY(s) KALPA_DO_STRINGIFY(s)
+#define KALPA_VARY_CONST_MOVE(f) \
+    f(const, &, KALPA_IDENTITY); \
+    f(, &, KALPA_IDENTITY); \
+    f(const, &&, std::move); \
+    f(, &&, std::move);
 
-#define KALPA_VERIFY(cond) verify(cond, KALPA_STRINGIFY(cond))
+
+template <typename T>
+using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
 
 
 }
--- src/result.h
+++ src/result.h
@@ -5,8 +5,38 @@
 #include <utility>
 #include <variant>
 
+#include "defs.h"
+
 
 namespace klp {
+
+
+template<typename V, typename E>
+class Result;
+
+
+enum ResultIndex {
+    ResultValueIndex,
+    ResultErrorIndex
+};
+
+
+template <typename E, typename V>
+Result<V, E> make_value(V value) {
+    return Result<V, E>(
+        std::in_place_index<ResultValueIndex>,
+        std::move(value)
+    );
+}
+
+
+template <typename V, typename E>
+Result<V, E> make_error(E error) {
+    return Result<V, E>(
+        std::in_place_index<ResultErrorIndex>,
+        std::move(error)
+    );
+}
 
 
 template <typename V, typename E>
@@ -18,70 +48,107 @@ public:
     using Value = V;
     using Error = E;
 
-    enum VariantIndex {
-        ValueIndex,
-        ErrorIndex
+    enum Index {
+        ValueIndex = ResultValueIndex,
+        ErrorIndex = ResultErrorIndex
     };
 
 public:
     using Base::Base;
 
-    Result(Value&& value) :
+    Result(Value value) :
         Base(std::in_place_index<ValueIndex>, std::move(value))
     {}
 
     template <
         typename H = void,
         typename = std::enable_if_t<!std::is_same_v<V, E>, H>
-    > Result(Error&& error) :
+    > Result(Error error) :
         Base(std::in_place_index<ErrorIndex>, std::move(error))
     {}
 
-    operator bool() const {
+    explicit operator bool() const {
         return Base::index() == ValueIndex;
     }
 
-    Value& operator*() {
-        return *std::get_if<ValueIndex>(this);
+    Value* value_ptr() {
+        return std::get_if<ValueIndex>(this);
     }
 
-    const Value& operator*() const {
-        return *std::get_if<ValueIndex>(this);
+    const Value* value_ptr() const {
+        return std::get_if<ValueIndex>(this);
     }
+
+#define OPERATOR_STAR(maybe_const, ref, maybe_move) \
+    maybe_const Value ref operator*() maybe_const ref { \
+        return maybe_move(*value_ptr()); \
+    }
+
+    KALPA_VARY_CONST_MOVE(OPERATOR_STAR)
+#undef OPERATOR_STAR
 
     Value* operator->() {
-        return &**this;
+        return value_ptr();
     }
 
     const Value* operator->() const {
-        return &**this;
+        return value_ptr();
     }
 
-    template <typename F>
-    auto map(F f) & {
-        return *this ? f(**this) : *this;
+    Error* error_ptr() {
+        return std::get_if<ErrorIndex>(this);
     }
 
-    template <typename F>
-    auto map(F f) const & {
-        return *this ? f(**this) : *this;
+    const Error* error_ptr() const {
+        return std::get_if<ErrorIndex>(this);
     }
 
-    template <typename F>
-    auto map(F f) && {
-        return *this ? f(std::move(**this)) : *this;
+#define ERROR(maybe_const, ref, maybe_move) \
+    maybe_const Error ref error() maybe_const ref { \
+        return maybe_move(*error_ptr()); \
     }
 
-    // TODO: map_err, and_then, etc..
+    KALPA_VARY_CONST_MOVE(ERROR)
+#undef ERROR
+
+#define MAP(maybe_const, ref, maybe_move) \
+    template <typename F> \
+    auto map(F f) maybe_const ref { \
+        return *this ? \
+            make_value<E>(f(maybe_move(**this))) : \
+            make_error<decltype(f(maybe_move(**this)))>(maybe_move(error())); \
+    }
+
+    KALPA_VARY_CONST_MOVE(MAP)
+#undef MAP
+
+#define MAP_ERROR(maybe_const, ref, maybe_move) \
+    template <typename F> \
+    auto map_error(F f) maybe_const ref { \
+        return *this ? \
+            make_value<decltype(f(maybe_move(error())))>(maybe_move(**this)) : \
+            make_error<V>(f(maybe_move(error()))); \
+    }
+
+    KALPA_VARY_CONST_MOVE(MAP_ERROR)
+#undef MAP_ERROR
+
+#define AND_THEN(maybe_const, ref, maybe_move) \
+    template <typename F> \
+    auto and_then(F f) maybe_const ref { \
+        return *this ? \
+            f(maybe_move(**this)) : \
+            decltype(f(maybe_move(**this)))(maybe_move(error())); \
+    }
+
+    KALPA_VARY_CONST_MOVE(AND_THEN)
+#undef AND_THEN
 };
 
 
-template <typename E, typename V = E>
-Result<V, E> make_error(E&& error) {
-    return Result<V, E>(
-        std::in_place_index<Result<V, E>::ErrorIndex>,
-        std::move(error)
-    );
+template <typename E>
+Result<E, E> as_error(E error) {
+    return make_error<E>(std::move(error));
 }
 
 
